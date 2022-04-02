f8c8c3f4636a60b141c024b5257e8fa732f03f2a
AustinMaZW <auzzyma@gmail.com>
2022-04-02 08:04:38 -0700

    first commit

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // parameter
+    [SerializeField] int breakableBlocks; // Serialized for debugging purposes
+                                          // Start is called before the first frame update
+    // cached reference
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+    public void CountBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <=0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //configuration parameters
+    [SerializeField] float ScreenWidthInUnits = 16f;
+    [SerializeField] float paddlePosMin = 1f;
+    [SerializeField] float paddlePosMax = 15f;
+
+    // cached references
+    GameSession theGameSession;
+    Ball theBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), paddlePosMin, paddlePosMax);
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * ScreenWidthInUnits;
+        }
+    }
+}
